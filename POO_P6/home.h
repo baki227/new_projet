04f1b426afc9f741f1b4618b583a9f6eadde3294
lol baki <WC.2726@outlook.fr>
2023-12-05 19:55:48 +0100

    nice disign for home

--- POO_P6/home.h
+++ POO_P6/home.h
@@ -47,6 +47,9 @@ namespace POOP6 {
 	private: System::Windows::Forms::Button^ button6;
 	private: System::Windows::Forms::Panel^ panel1;
 	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Panel^ panel3;
+	private: System::Windows::Forms::Label^ label2;
 	protected:
 
 	private:
@@ -71,15 +74,21 @@ namespace POOP6 {
 			this->button6 = (gcnew System::Windows::Forms::Button());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->panel1->SuspendLayout();
 			this->panel2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
 			this->button1->BackColor = System::Drawing::Color::Goldenrod;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 9));
 			this->button1->ForeColor = System::Drawing::Color::White;
-			this->button1->Location = System::Drawing::Point(124, 376);
+			this->button1->Location = System::Drawing::Point(142, 471);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(196, 79);
 			this->button1->TabIndex = 0;
@@ -89,47 +98,66 @@ namespace POOP6 {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(524, 34);
+			this->button2->BackColor = System::Drawing::Color::Goldenrod;
+			this->button2->FlatAppearance->BorderSize = 0;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->ForeColor = System::Drawing::Color::White;
+			this->button2->Location = System::Drawing::Point(142, 216);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(196, 79);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Gestion des clients";
-			this->button2->UseVisualStyleBackColor = true;
+			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &home::gestionClient_Click);
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(124, 458);
+			this->button3->BackColor = System::Drawing::Color::Goldenrod;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->ForeColor = System::Drawing::Color::White;
+			this->button3->Location = System::Drawing::Point(425, 352);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(196, 79);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"Gestion des commandes";
-			this->button3->UseVisualStyleBackColor = true;
+			this->button3->UseVisualStyleBackColor = false;
 			this->button3->Click += gcnew System::EventHandler(this, &home::gestionCommandes_Click);
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(524, 204);
+			this->button4->BackColor = System::Drawing::Color::Goldenrod;
+			this->button4->FlatAppearance->BorderSize = 0;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->ForeColor = System::Drawing::Color::White;
+			this->button4->Location = System::Drawing::Point(142, 352);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(196, 79);
 			this->button4->TabIndex = 3;
 			this->button4->Text = L"Gestion du stock";
-			this->button4->UseVisualStyleBackColor = true;
+			this->button4->UseVisualStyleBackColor = false;
 			this->button4->Click += gcnew System::EventHandler(this, &home::gestionStock_Click);
 			// 
 			// button5
 			// 
-			this->button5->Location = System::Drawing::Point(524, 289);
+			this->button5->BackColor = System::Drawing::Color::Goldenrod;
+			this->button5->FlatAppearance->BorderSize = 0;
+			this->button5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button5->ForeColor = System::Drawing::Color::White;
+			this->button5->Location = System::Drawing::Point(425, 216);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(196, 79);
 			this->button5->TabIndex = 4;
 			this->button5->Text = L"Gestion des statistiques";
-			this->button5->UseVisualStyleBackColor = true;
+			this->button5->UseVisualStyleBackColor = false;
 			this->button5->Click += gcnew System::EventHandler(this, &home::gestionStatistique_Click);
 			// 
 			// button6
 			// 
 			this->button6->BackColor = System::Drawing::Color::Goldenrod;
+			this->button6->Cursor = System::Windows::Forms::Cursors::Default;
+			this->button6->FlatAppearance->BorderSize = 0;
+			this->button6->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->button6->ForeColor = System::Drawing::Color::White;
 			this->button6->Location = System::Drawing::Point(666, 613);
 			this->button6->Name = L"button6";
@@ -142,6 +170,8 @@ namespace POOP6 {
 			// panel1
 			// 
 			this->panel1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel1.BackgroundImage")));
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Controls->Add(this->panel3);
 			this->panel1->Location = System::Drawing::Point(0, -2);
 			this->panel1->Name = L"panel1";
 			this->panel1->Size = System::Drawing::Size(401, 715);
@@ -150,7 +180,11 @@ namespace POOP6 {
 			// panel2
 			// 
 			this->panel2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel2.BackgroundImage")));
+			this->panel2->Controls->Add(this->label2);
 			this->panel2->Controls->Add(this->button3);
+			this->panel2->Controls->Add(this->button4);
+			this->panel2->Controls->Add(this->button5);
+			this->panel2->Controls->Add(this->button2);
 			this->panel2->Controls->Add(this->button6);
 			this->panel2->Controls->Add(this->button1);
 			this->panel2->Location = System::Drawing::Point(400, -2);
@@ -158,15 +192,47 @@ namespace POOP6 {
 			this->panel2->Size = System::Drawing::Size(782, 670);
 			this->panel2->TabIndex = 7;
 			// 
+			// panel3
+			// 
+			this->panel3->BackColor = System::Drawing::Color::Transparent;
+			this->panel3->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel3.BackgroundImage")));
+			this->panel3->Location = System::Drawing::Point(29, 168);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(354, 114);
+			this->panel3->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Small", 11, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(136, 285);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(128, 30);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Yes We Are";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(121, 82);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(563, 27);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Welcome to the menu dedicated to our customer service team.";
+			// 
 			// home
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 21);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1182, 667);
 			this->Controls->Add(this->panel1);
-			this->Controls->Add(this->button5);
-			this->Controls->Add(this->button4);
-			this->Controls->Add(this->button2);
 			this->Controls->Add(this->panel2);
 			this->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 8, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
@@ -175,7 +241,10 @@ namespace POOP6 {
 			this->Name = L"home";
 			this->Text = L"home";
 			this->Load += gcnew System::EventHandler(this, &home::home_Load);
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
